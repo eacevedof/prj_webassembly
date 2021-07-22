3e3331cb44284b55ffcdeb82cfd1478c81f93039
ioedumac <eacevedof@yahoo.es>
2021-07-22 19:36:15 +0200

    async-example con public

--- rust/async-example/src/main.rs
+++ rust/async-example/src/main.rs
@@ -1,10 +1,14 @@
 use wasm_bindgen::prelude::*;
-use std::env;
 
+fn main() {
+  println!("message: {}", ":)");
+}
 
 #[wasm_bindgen]
-pub fn get_data(url: &str) -> str {
-  return "hola"
+pub fn get_data(url: &str) -> String {
+  let mut string = String::new();
+  string.push_str("xxxx");
+  return string;
 }
 
 //compilar:
