3aa674ec407bc03ad81e566be7bfc2d7c3f0148f
ioedumac <eacevedof@yahoo.es>
2021-07-22 19:40:48 +0200

    async no tira no imprime el string

--- rust/async-example/src/main.rs
+++ rust/async-example/src/main.rs
@@ -1,11 +1,7 @@
 use wasm_bindgen::prelude::*;
 
-fn main() {
-  println!("message: {}", ":)");
-}
-
 #[wasm_bindgen]
-pub fn get_data(url: &str) -> String {
+pub fn get_data() -> String {
   let mut string = String::new();
   string.push_str("xxxx");
   return string;
