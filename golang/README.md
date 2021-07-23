7c53a1ea14727ae0ed5a692c57e4354363b0b939
ioedumac <eacevedof@yahoo.es>
2021-07-23 16:50:08 +0200

    no tira go env GOOOT

---
+++ golang/README.md
@@ -0,0 +1,9 @@
+- **instalar go**
+```
+mkdir -p $HOME/go/{bin,src}
+
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+
+. ~/.bash_profile
+```
--- golang/hello-world/main.go
+++ golang/hello-world/main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"syscall/js"
-)
-
-// Add ...
-func Add(a, b int) int {
-	return a + b
-}
+import "fmt"
 
 func main() {
-	c := make(chan struct{}, 0)
-
-	add := func(args []js.Value) {
-		go func() {
-			js.Global().Set("output", js.ValueOf(Add(args[0].Int(), args[1].Int())))
-			close(c)
-		}()
-
-	}
-
-	js.Global().Set("add", js.NewCallback(add))
-
-	<-c
+	fmt.Println("Hello, WebAssembly!")
 }
