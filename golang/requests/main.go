481830f1654d9be2ae099472ab7943b275363477
ioedumac <eacevedof@yahoo.es>
2021-08-06 12:32:47 +0200

    llamar funcs de go

--- golang/requests/main.go
+++ golang/requests/main.go
@@ -2,10 +2,31 @@ package main
 
 import (
     "fmt"
-	"request"
+	//"request"
+	"syscall/js"
 )
 
+var channel chan bool
+
+func printM(this js.Value, inputs []js.Value) interface{} {
+    fmt.Println("printer")
+    channel <- true
+
+    return this
+}
+
+func init() {
+    channel = make(chan bool)
+}
+
 func main() {
-    strproducts := request.FetchProducts()
-    fmt.Println(strproducts)
+    //interact js from go
+    //strproducts := request.FetchProducts()
+    //fmt.Println(strproducts)
+    fmt.Println("returning")
+
+    // func main must have no arguments and no return values
+    //return strproducts
+    js.Global().Set("printM", js.FuncOf(printM))
+    <-channel
 }
