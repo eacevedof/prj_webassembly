e1b8ed56f02982ea4063f579771438e7f6cbf160
ioedumac <eacevedof@yahoo.es>
2021-09-05 16:25:04 +0200

    nok backup main.go

--- golang/requests/main.go
+++ golang/requests/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"request"
 	"syscall/js"
 )
@@ -10,11 +11,16 @@ var channel chan bool
 //tutorial
 //https://tutorialedge.net/golang/go-webassembly-tutorial/
 
-func get_products(inputs []js.Value) {
-	function := inputs[len(inputs)-1:][0]
+func get_products() {
 	strproducts := request.FetchProducts()
-	//fmt.Println(strproducts)
-	function.Invoke(js.Null(), strproducts)
+	fmt.Println(strproducts)
+	//function.Invoke(js.Null(), strproducts)
+
+}
+
+func add(i []js.Value) {
+	js.Global().Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
+	println(js.ValueOf(i[0].Int() + i[1].Int()).String())
 }
 
 func main() {
@@ -22,7 +28,7 @@ func main() {
 	//abre listener de ejecucion
 	channel = make(chan bool)
 
-	js.Global().Set("get_products", js.FuncOf(get_products))
+	js.Global().Set("get_products", js.Callback(get_products))
 	//cierra el canal
 	<-channel
 
